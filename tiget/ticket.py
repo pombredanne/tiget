c7ab778b3e9f496bc754990a760ba335d8037fab
Martin Natano <natano@natano.net>
2012-08-19 10:11:13 +0200

    remove unused method

--- tiget/ticket.py
+++ tiget/ticket.py
@@ -7,17 +7,6 @@ class Ticket(Model):
         'description': TextField(),
     }
 
-    def get_file_content(self):
-        content = u''
-        for name in ('summary', 'description'):
-            field = self.fields[name]
-            if field.hidden:
-                continue
-            value = self.data.get(name, field.default) or u''
-            value = value.replace(u'\n', u'\n    ')
-            content += u'{0}: {1}\n'.format(name, value)
-        return content
-
     def edit(self):
         s = edit_content(self.serialize())
         self.deserialize(s)
