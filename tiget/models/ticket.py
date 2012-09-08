ad9e1c937e62fb6fe0bfc7983afe877e98619262
Martin Natano <natano@natano.net>
2012-09-08 23:50:07 +0200

    add milestone and owner to ticket

--- tiget/models/ticket.py
+++ tiget/models/ticket.py
@@ -1,11 +1,15 @@
 from tiget.models.base import Model
-from tiget.models.fields import TextField
+from tiget.models.fields import TextField, ForeignKey
 from tiget.utils import open_in_editor
+from tiget.models.user import User
+from tiget.models.milestone import Milestone
 
 
 class Ticket(Model):
     summary = TextField()
     description = TextField(null=True)
+    milestone = ForeignKey(Milestone, null=True)
+    owner = ForeignKey(User, null=True)
 
     def open_in_editor(self):
         s = open_in_editor(self.dumps())
