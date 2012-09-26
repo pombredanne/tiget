0229f02f9cc7905dd53f043a34b34e2fd3b1c9cc
Martin Natano <natano@natano.net>
2012-09-26 10:55:38 +0200

    remove run_tiget.py; use ./setup.py develop instead

--- run_tiget.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os, sys
-
-if not __name__ == '__main__':
-    raise RuntimeError('You are trying to import the wrapper script.')
-
-project_root = os.path.normpath(os.path.dirname(sys.argv[0]))
-sys.path.insert(0, project_root)
-execfile(os.path.join(project_root, 'scripts', 'tiget'))
