bd702600d14493ce759367ece57a6979130c6b31
Martin Natano <natano@natano.net>
2012-09-30 23:21:39 +0200

    kill embedded ipython shell when reloading

--- plugins/tiget_ipython/tiget_ipython/__init__.py
+++ plugins/tiget_ipython/tiget_ipython/__init__.py
@@ -5,6 +5,9 @@ __version__ = '0.1a0'
 
 
 def init_plugin(plugin):
+    from IPython.frontend.terminal import embed
+    embed._embedded_shell = None
+
     from tiget_ipython.cmds import ipython_cmd
     plugin.add_cmd(ipython_cmd)
     plugin.add_setting('prompt', StrSetting(default='IPython[\\#]> '))
