b2b7f14a65a52143c9503cb254efd57652645a45
Martin Natano <natano@natano.net>
2012-10-01 18:42:44 +0200

    search for repository in setup_repository

--- tiget/setup_repository.py
+++ tiget/setup_repository.py
@@ -1,9 +1,10 @@
-from tiget.git import init_repo, GitError
+from tiget.git import find_repository_path, init_repo, GitError
 from tiget.utils import print_error
 
 
 def main():
     try:
+        settings.core.repository_path = find_repository_path()
         init_repo()
     except GitError as e:
         print_error(e)
