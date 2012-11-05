8f828e804b1c49295b7fec421073845e0fb31764
Martin Natano <natano@natano.net>
2012-11-05 23:19:50 +0100

    add more tests for tiget.git

--- tiget/tests/test_git.py
+++ tiget/tests/test_git.py
@@ -13,56 +13,162 @@ class GitTestcase(object):
         self.repo = mkdtemp()
         check_call(['git', 'init', '--quiet'], cwd=self.repo)
         settings.core.repository = self.repo
+        self.branchref = 'refs/heads/{}'.format(settings.core.branchname)
+        self.assert_commit_count(0)
 
     def teardown(self):
         shutil.rmtree(self.repo)
 
-    def assert_file_exists(self, file):
+    def assert_file_exists(self, filename):
         cmd = 'git ls-tree --name-only -r {} | grep -q ^{}$'.format(
-            settings.core.branchname, file)
+            self.branchref, filename)
         check_call(cmd, cwd=self.repo, shell=True)
 
     def assert_commit_count(self, count):
-        cmd = 'git log --oneline {} | wc -l'.format(settings.core.branchname)
+        cmd = 'git log --oneline {} 2>/dev/null| wc -l'.format(self.branchref)
         output = check_output(cmd, cwd=self.repo, shell=True).decode('utf-8')
         eq_(output.strip(), str(count))
 
 
-class TestGit(GitTestcase):
+class TestInitRepo(GitTestcase):
     def test_init_repo(self):
         init_repo()
-        ref = 'refs/heads/{}'.format(settings.core.branchname)
         check_call(
-            ['git', 'show-ref', '--verify', '--quiet', ref], cwd=self.repo)
+            ['git', 'show-ref', '--verify', '--quiet', self.branchref],
+            cwd=self.repo)
         self.assert_commit_count(1)
         self.assert_file_exists('config/VERSION')
 
-    @transaction.wrap()
+
+class TestTransaction(GitTestcase):
+    def test_commit(self):
+        transaction.begin()
+        trans = transaction.current(initialized=False)
+        trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+        trans.add_message('foobar')
+        self.assert_commit_count(0)
+        transaction.commit()
+        self.assert_commit_count(1)
+        self.assert_file_exists('foo')
+
+    def test_commit_nochanges(self):
+        transaction.begin()
+        self.assert_commit_count(0)
+        with assert_raises(GitError):
+            transaction.commit()
+        self.assert_commit_count(0)
+        transaction.rollback()
+
+    def test_commmit_nomessage(self):
+        transaction.begin()
+        trans = transaction.current(initialized=False)
+        trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+        self.assert_commit_count(0)
+        with assert_raises(GitError):
+            transaction.commit()
+        self.assert_commit_count(0)
+        transaction.rollback()
+
+    def test_rollback(self):
+        class TestException(Exception): pass
+
+        with assert_raises(TestException):
+            with transaction.wrap():
+                trans = transaction.current(initialized=False)
+                trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+                raise TestException()
+        self.assert_commit_count(0)
+
     def test_transaction_current(self):
         with assert_raises(GitError):
             transaction.current(initialized=True)
-        transaction.current(initialized=False)
-        init_repo()
-        transaction.current(initialized=True)
         with assert_raises(GitError):
             transaction.current(initialized=False)
-
-    def test_transaction_commit(self):
-        init_repo()
         with transaction.wrap():
-            trans = transaction.current()
+            with assert_raises(GitError):
+                transaction.current(initialized=True)
+            transaction.current(initialized=False)
+            init_repo()
+            transaction.current(initialized=True)
+            with assert_raises(GitError):
+                transaction.current(initialized=False)
+
+
+class TestTransactionWrap(GitTestcase):
+    def test_wrap(self):
+        with transaction.wrap():
+            trans = transaction.current(initialized=False)
             trans.set_blob(['foo'], 'bar'.encode('utf-8'))
             trans.add_message('foobar')
-        self.assert_commit_count(2)
+            self.assert_commit_count(0)
+        self.assert_commit_count(1)
         self.assert_file_exists('foo')
 
-    def test_transaction_rollback(self):
+    def test_nochanges(self):
+        with transaction.wrap():
+            pass
+        self.assert_commit_count(0)
+
+    def test_exception(self):
         class TestException(Exception): pass
 
-        init_repo()
         with assert_raises(TestException):
             with transaction.wrap():
-                trans = transaction.current()
+                trans = transaction.current(initialized=False)
                 trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+                self.assert_commit_count(0)
                 raise TestException()
-        self.assert_commit_count(1)
+        self.assert_commit_count(0)
+
+
+class TestTransactionBlob(GitTestcase):
+    def test_set_get(self):
+        with transaction.wrap():
+            trans = transaction.current(initialized=False)
+            trans.add_message('dummy')
+            trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+            eq_(trans.get_blob(['foo']).decode('utf-8'), 'bar')
+        self.assert_file_exists('foo')
+
+    def test_set_get_separate_transactions(self):
+        with transaction.wrap():
+            trans = transaction.current(initialized=False)
+            trans.add_message('dummy')
+            trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+        self.assert_file_exists('foo')
+        with transaction.wrap():
+            trans = transaction.current()
+            eq_(trans.get_blob(['foo']).decode('utf-8'), 'bar')
+        self.assert_file_exists('foo')
+
+    def test_exists(self):
+        with transaction.wrap():
+            trans = transaction.current(initialized=False)
+            trans.add_message('dummy')
+            trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+            ok_(trans.exists(['foo']))
+
+    def test_exists_separate_transaction(self):
+        with transaction.wrap():
+            trans = transaction.current(initialized=False)
+            trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+            trans.add_message('dummy')
+        with transaction.wrap():
+            trans = transaction.current()
+            ok_(trans.exists(['foo']))
+
+    def test_list_blobs(self):
+        with transaction.wrap():
+            trans = transaction.current(initialized=False)
+            trans.add_message('dummy')
+            trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+            ok_(trans.list_blobs([]) == set(['foo']))
+
+    def test_list_blobs_separate_transaction(self):
+        with transaction.wrap():
+            trans = transaction.current(initialized=False)
+            trans.set_blob(['foo'], 'bar'.encode('utf-8'))
+            trans.add_message('dummy')
+        with transaction.wrap():
+            trans = transaction.current()
+            ok_(trans.list_blobs([]) == set(['foo']))
