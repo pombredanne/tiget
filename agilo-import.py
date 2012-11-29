c9952bebdf13562068ba23c6d3f73645e8f0a221
Martin Natano <natano@natano.net>
2012-11-29 23:04:12 +0100

    add Sprint model to scrum plugin and improve agilo-importer

--- agilo-import.py
+++ agilo-import.py
@@ -4,34 +4,65 @@ import getopt
 from datetime import datetime, timezone
 
 import psycopg2
+from psycopg2.extras import DictCursor
 
 from tiget.git import transaction
-from tiget.scrum.models import Milestone, Ticket
-
-
-def make_datetime(timestamp):
-    return datetime.fromtimestamp(timestamp, timezone.utc)
+from tiget.scrum.models import Milestone, Sprint, Ticket
 
 
 @transaction.wrap('agilo import')
 def main():
     conn = psycopg2.connect(' '.join(sys.argv[1:]))
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=DictCursor)
 
     print('Importing Milestones')
-    cur.execute('select name, description, completed from milestone')
-    for name, description, completed in cur:
-        Milestone.objects.create(name=name, description=description,
-            completed_at=make_datetime(completed))
+    cur.execute('''
+        select name,
+               description,
+               to_timestamp(due) as due,
+               to_timestamp(completed) as completed_at
+        from milestone
+    ''')
+    for row in cur:
+        Milestone.objects.create(**row)
+
+    print('Importing Sprints')
+    cur.execute('''
+        select name,
+               description,
+               case milestone when '' then null else milestone end as milestone,
+               to_timestamp(start) as start,
+               to_timestamp(sprint_end) as end
+        from agilo_sprint
+    ''')
+    for row in cur:
+        Sprint.objects.create(**row)
 
     print('TODO: import users')
 
     print('Importing Tickets')
-    cur.execute('select summary, description, milestone from ticket')
-    for summary, description, milestone in cur:
-        milestone = Milestone.objects.get(name=milestone) if milestone else None
-        Ticket.objects.create(summary=summary, description=description,
-            milestone=milestone)
+    cur.execute('''
+        select summary,
+               description,
+               case milestone when '' then null else milestone end as milestone,
+               case type when 'task' then 'feature' else type end as ticket_type,
+               case when status in ('assigned', 'accepted', 'reopened') then 'new'
+                    when status in ('info_needed', 'undecided') then 'wtf'
+                    when status = 'closed' then
+                        case resolution when 'invalid' then 'invalid'
+                                        when 'worksforme' then 'invalid'
+                                        when 'duplicate' then 'duplicate'
+                                        when 'wontfix' then 'wontfix'
+                                        else 'fixed'
+                        end
+                    else status
+               end as status
+        from ticket
+        where type in ('idea', 'requirement', 'bug', 'feature', 'wording',
+                       'story', 'task', 'training')
+    ''')
+    for row in cur:
+        Ticket.objects.create(**row)
 
     cur.close()
     conn.close()
--- tiget/scrum/models.py
+++ tiget/scrum/models.py
@@ -13,7 +13,16 @@ class User(models.Model):
 class Milestone(models.Model):
     name = models.TextField(primary_key=True)
     description = models.TextField(null=True)
+    due = models.DateTimeField(null=True)
     completed_at = models.DateTimeField(null=True)
+
+
+class Sprint(models.Model):
+    name = models.TextField(primary_key=True)
+    description = models.TextField(null=True)
+    milestone = models.ForeignKey(Milestone, null=True)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
 
 
 def get_current_user():
@@ -24,9 +33,29 @@ def get_current_user():
 
 
 class Ticket(models.Model):
+    STATUS_CHOICES = (
+        'new',
+        'testing',
+        'fixed',
+        'invalid',
+        'wontfix',
+        'duplicate',
+        'wtf',
+    )
+    TYPE_CHOICES = (
+        'bug',
+        'feature',
+        'idea',
+        'training',
+        'story',
+        'requirement',
+        'wording',
+    )
     summary = models.TextField()
     description = models.TextField(null=True)
     milestone = models.ForeignKey(Milestone, null=True)
+    sprint = models.ForeignKey(Sprint, null=True)
     reporter = models.ForeignKey(User, default=get_current_user)
     owner = models.ForeignKey(User, null=True)
-    status = models.TextField(choices=('new', 'testing', 'done'), default='new')
+    status = models.TextField(choices=STATUS_CHOICES, default='new')
+    ticket_type = models.TextField(choices=TYPE_CHOICES, default='bug')
