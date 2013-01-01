26577a435edcd00710f84a649e3bb857575bdb33
Martin Natano <natano@natano.net>
2013-01-01 22:12:51 +0100

    Add mine cmd to scrum plugin

--- tiget/scrum/__init__.py
+++ tiget/scrum/__init__.py
@@ -7,9 +7,8 @@ __version__ = tiget.__version__
 
 
 def load(plugin):
-    from tiget.scrum import models
-    from tiget.scrum.cmds import accept_cmd
+    from tiget.scrum import models, cmds
     plugin.add_models(models)
-    plugin.add_cmd(accept_cmd)
+    plugin.add_cmds(cmds)
     plugin.add_setting(
         'current_user', StrSetting(default=get_config('user.email')))
--- tiget/scrum/cmds.py
+++ tiget/scrum/cmds.py
@@ -1,7 +1,8 @@
 from tiget.cmds import cmd, CmdError
 from tiget.git import transaction
 from tiget.git.models import get_model
-
+from tiget.utils import paginate
+from tiget.table import Table
 from tiget.scrum.models import Ticket, User
 
 
@@ -20,3 +21,28 @@ def accept_cmd(opts, ticket_id):
     except (Ticket.DoesNotExist, User.DoesNotExist) as e:
         raise CmdError(e)
     ticket.save()
+
+
+@cmd(options='a')
+@transaction.wrap()
+def mine_cmd(opts):
+    """
+    list tickets owned by the current user
+
+    SYNOPSIS
+        mine [-a]
+    """
+    show_all = False
+    for opt, arg in opts:
+        if opt == '-a':
+            show_all = True
+    tickets = Ticket.objects.filter(owner=User.current())
+    if not show_all:
+        tickets = tickets.filter(status__in=('new', 'wtf'))
+    field_names = ('summary', 'milestone', 'sprint', 'status', 'ticket_type')
+    fields = [Ticket._meta.get_field(f) for f in field_names]
+    table = Table(*(f.name for f in fields))
+    for ticket in tickets:
+        values = [f.dumps(getattr(ticket, f.attname)) for f in fields]
+        table.add_row(*values)
+    paginate(table.render())
