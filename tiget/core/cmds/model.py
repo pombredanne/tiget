f0c51929aa4b158ad48dd3547b08281271f7e67b
Martin Natano <natano@natano.net>
2013-01-26 20:48:23 +0100

    clean up list cmd

--- tiget/core/cmds/model.py
+++ tiget/core/cmds/model.py
@@ -59,19 +59,19 @@ class List(Cmd):
 
     def setup(self):
         self.parser.add_argument('-f', '--fields')
-        self.parser.add_argument('-s', '--sort')
-        self.parser.add_argument('-l', '--limit', type=int)
+        self.parser.add_argument('-o', '--order', default='pk')
+        self.parser.add_argument('-s', '--slice', type=self.parse_slice)
         self.parser.add_argument('model', type=model_type)
+
+    def parse_slice(self, value):
+        parts = [int(part) if part else None for part in value.split(':')]
+        return slice(*parts)
 
     @transaction.wrap()
     def do(self, args):
-        objs = args.model.objects.all()
-        if args.sort:
-            objs = objs.order_by(*args.sort.split(','))
-        if not args.limit is None:
-            objs = objs[:args.limit]
-        fields = None
-        if args.fields:
-            fields = args.fields.split(',')
+        objs = args.model.objects.order_by(*args.order.split(','))
+        if args.slice:
+            objs = objs[args.slice]
+        fields = args.fields.split(',') if args.fields else None
         table = Table.from_queryset(objs, fields=fields)
         self.print(table.render())
