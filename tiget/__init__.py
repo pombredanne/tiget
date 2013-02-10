470f04be4d67cfdfab9cdc89cd72f0b8ad51a596
Martin Natano <natano@natano.net>
2013-02-10 16:24:30 +0100

    add __version__ to tiget/__init__.py

--- tiget/__init__.py
+++ tiget/__init__.py
@@ -1,5 +1,8 @@
 from tiget.plugins import load_plugin
 
+from tiget.version import VERSION as __version__
+__author__ = 'Martin Natano <natano@natano.net>'
+
 
 BUILTIN_PLUGINS = ('core', 'importer', 'scrum')
 
