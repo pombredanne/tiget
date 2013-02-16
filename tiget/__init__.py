bc8583aecc84ba09864d5c76dd29b538e95593d6
Martin Natano <natano@natano.net>
2013-02-16 21:31:18 +0100

    use full path for BUILTIN_PLUGINS

--- tiget/__init__.py
+++ tiget/__init__.py
@@ -4,7 +4,7 @@ from tiget.version import VERSION as __version__
 __author__ = 'Martin Natano <natano@natano.net>'
 
 
-BUILTIN_PLUGINS = ('core', 'importer', 'scrum')
+BUILTIN_PLUGINS = ('tiget.core', 'tiget.git.importer', 'tiget.scrum')
 
 for plugin_name in BUILTIN_PLUGINS:
     load_plugin(plugin_name)
